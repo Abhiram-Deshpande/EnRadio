b9d4cde9a4568a69f3f87ed16204a4d7ee215a86
Abhiram Deshpande <designing.developer111@gmail.com>
2023-12-02 13:56:27 +0530

    Bug fix: <NavBar> Working properly

--- project-frontend/src/components/Navbar/Navbar.js
+++ project-frontend/src/components/Navbar/Navbar.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "./navbar.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import website_logo from '../../image/website-logo.jpg'
+import button_style from './ButtonStyles.css'
+import image from '../../image/website-logo.jpg'
 
 // React material Ui Button
 import Button from '@material-ui/core/Button';
@@ -10,68 +12,60 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [showMediaIcons, setShowMediaIcons] = useState(false);
+  const linkButtonStyle=
+  {
+    fontSize: '18px',
+    color:"black",
+    fontWeight:"500",
+    padding:"12px"
+}
   return (
     <>
       <nav className="main-nav">
-        <div className="logo">
+        <div className="logo-container">
         <NavLink to="/">
+          <img className="website-logo" src={image} alt="Something went wrong"/>
         </NavLink>  
         </div>
-
-        <div
-          className={
-            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
-          }
-        >
+        <div className={showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"}>
           <ul>
             <li >
-              <NavLink to="/"><Button variant="primary" size="large" style={{fontSize: '15px',color:"black",fontWeight:"500"}}>Home</Button></NavLink>
+              <NavLink to="/"><Button variant="primary" size="large" style={linkButtonStyle}>Home</Button></NavLink>
             </li> 
             
             <li className="">
-              <NavLink to="/service"><Button variant="primary" size="large" style={{fontSize: '15px',color:"black",fontWeight:"500" }}>Services</Button></NavLink>
-            
-            </li>
-            <li className="">
-              <NavLink to="/login"><Button variant="primary" size="large" style={{fontSize: '15px',color:"black",fontWeight:"500"}}>LOGIN</Button></NavLink>
-             
-            </li>
-             
-            <li className="">
-              <NavLink to="/about"><Button variant="primary" size="large" style={{fontSize: '15px',color:"black",fontWeight:"500"}}>ABOUT</Button></NavLink>
-             
+              <NavLink to="/service">
+                <Button variant="primary" size="large" style={linkButtonStyle}>
+                  Services
+                </Button>
+              </NavLink>
             </li>
 
+            <li className="">
+              <NavLink to="/try-it-out">
+                <Button variant="primary" size="large" style={linkButtonStyle}>
+                  Try it Out
+                </Button>
+              </NavLink>
+            </li>
+             
+            <li className="">
+              <NavLink to="/about">
+                <Button variant="primary" size="large" style={linkButtonStyle}>
+                  ABOUT
+                </Button>
+              </NavLink>
+            </li>
           </ul>
+
         </div>
-
-        <div className="social-media">
-          <ul className="social-media-desktop">
-            <li>
-              <a href="https://www.instagram.com/" target="avishkar">
-                <img href="%PUBLIC_URL%/svg/instagram-svg.svg"></img>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.youtube.com/watch?v=ES1XZ-N6w3A&ab_channel=NischayMalhan"
-                target="_avishkar"
-              >
-              </a>
-            </li>
-          </ul>
-
-          <div className="hamburger-menu">
-            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
-              <GiHamburgerMenu />
-            </a>
-          </div>
-   
+        <div class="loginButton-container">
+        <button style={button_style.loginButton} id="loginButton" >
+          <NavLink to="/login"></NavLink>
+          Login
+        </button>
         </div>
       </nav>
-
-
-
     </>
   );
 };
